f848b505bd46fd69b3313f088cba7cd203ddfd25
Michael Johnson <m.ianjohnson@gmail.com>
2017-08-11 16:38:27 -0400

    timer and loading revision., Live update button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,34 @@ $(document).ready(function(){
 	var today = moment().format('dddd, MMMM Do YYYY');
 	$('#date').html(today);
 
+	function fetchCard() {
+		$('#panel').append('<span id="loading"><i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i></span>');
+
+		setTimeout(function(){
+			$.ajax({
+					type:'GET',
+					url:'get_squares.php',
+					dataType: "json",
+					success:function(data){
+							// console.log(data);
+							$('#panel').empty();
+							$.each( data, function(index, panel) {
+								$('#panel').append('<li id="' + panel.id + '"><span>' + panel.block + '</span></li>');
+							});
+
+							var freespace = $('ul#panel').find('li:eq(11)');
+							$('<li id="free"></li>').insertAfter(freespace);
+
+							$('li').click(function(){
+								$(this).toggleClass('select');
+					    });
+					}
+			});
+		}, 2000);
+	}
+
 	// Read / Format the JSON array from the PHP server output
-	$.ajax({
-			type:'GET',
-			url:'get_squares.php',
-			dataType: "json",
-			success:function(data){
-					// console.log(data);
-					$.each( data, function(index, panel) {
-						$('#panel').append('<li id="' + panel.id + '"><span>' + panel.block + '</span></li>');
-					});
-
-					var freespace = $('ul#panel').find('li:eq(11)');
-					$('<li id="free"></li>').insertAfter(freespace);
-
-					$('li').click(function(){
-						$(this).toggleClass('select');
-			    });
-			}
-	});
+	fetchCard();
 
 	/* Google Sheets API
 	var sheetId = '1e3QmEU6BrlpkUmbaviNfwGx0vpfOL740Nwj8gDboTBQ';
@@ -39,6 +48,11 @@ $(document).ready(function(){
 		});
 	});
 	*/
+
+	$('#new-board').on('click', function() {
+		$('#panel').empty();
+		fetchCard();
+	});
 
 	dialog = $( "#request" ).dialog({
 		modal: true,
