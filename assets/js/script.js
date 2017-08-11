4f27ebf24f64015806916ca1e8bf70c0c43c05c6
Michael Johnson <m.ianjohnson@gmail.com>
2017-08-11 00:47:29 -0400

    final gSheets sync

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,11 +25,20 @@ $(document).ready(function(){
 
 	*/
 
-	console.log('boo');
-
-	$.getJSON("http://cors.io/spreadsheets.google.com/feeds/list/1e3QmEU6BrlpkUmbaviNfwGx0vpfOL740Nwj8gDboTBQ/od6/public/values?alt=json", function(data) {
+	var sheetId = '1e3QmEU6BrlpkUmbaviNfwGx0vpfOL740Nwj8gDboTBQ';
+	$.getJSON("https://spreadsheets.google.com/feeds/list/"+ sheetId +"/1/public/values?alt=json", function(data) {
   //first row "title" column
-  	console.log(data.feed.entry[0]['gsx$title']['$t']);
+		$.each(data.feed.entry, function(key, cell) {
+					console.log(cell.title.$t);
+					$('#panel').append('<li id="' + cell.title.$t + '"><span>' + cell.title.$t + '</span></li>');
+		});
+
+		var freespace = $('ul#panel').find('li:eq(11)');
+		$('<li id="free"></li>').insertAfter(freespace);
+
+		$('li').click(function(){
+			$(this).toggleClass('select');
+		});
 	});
 
 	dialog = $( "#request" ).dialog({
